833d9beebe65b6a97baa8a979d07fb495bb5604a
Imran Afick <Iafick@student.cscc.edu>
2026-07-14 23:57:37 -0400

    Add settings class for game configuration

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,43 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
     def __init__(self):
+        """Initialize the game and create game resources."""
+
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+             self.settings.screen_height)
+        )
+
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
+        """Start the main loop for the game."""
+
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             pygame.display.flip()
 
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
 
 if __name__ == '__main__':
-    game = AlienInvasion()
-    game.run_game()
-        
+    # Make a game instance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
