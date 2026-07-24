fe9a3cb4c39a12e35c8fc9570c2611b8264064de
Iafick <iafick@student.cscc.edu>
2026-07-23 21:32:18 -0400

    Update game settings and initialization methods
    Refactor settings for game parameters and initialize methods.

--- settings.py
+++ settings.py
@@ -9,74 +9,31 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-         # Ship settings
-        self.ship_speed = 1.5
-        # Game settings
-        self.title = "Alien Invasion"
+        self.bullet_width = 300
+        self.bullet_height = 15
+        self.bullet_color = (60, 60,60)
+        self..bullets_allowed = 5
 
-
-        from pathlib import Path
-class Settings:
-    
-    
-    def __init__(self):
-        """Initialize the game's static settings."""
-        self.name: str = "Alien Invasion"
-        self.screen_w = 1200
-        self.screen_h = 800
-        #frames per second
-        self.FPS = 60
-        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
-        self.scores_file = Path.cwd() / 'Assets' / 'file' / 'scores.json'
-
-        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship.png'
-        self.ship_w = 75          
-        self.ship_h = 115          
-      
-
-        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
-        self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
-        self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
-        self.fleet_step_sound = Path.cwd() / 'Assets' / 'sound' / 'fleet_step.wav'
-        
-        
-
-        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy.png'
-        self.alien_w  = 40 
-        self.alien_h = 40  
-        self.fleet_direction = 1 
         self.fleet_drop_speed = 10
 
-        self.button_w = 200
-        self.button_h = 50
-        self.button_color = (64, 57, 138)               # Indigo Fade
-        self.button_hover_color = (90, 83, 193)       # Hover background         
-        self.button_glow_color = (173, 141, 255)    # Soft Lavender (outline glow)
-        self.button_hover_glow_color = (198, 169, 255)    # Hover border
-        self.text_color = (244, 241, 251)         # Snow Lavender (text)
-        
-        self.button_font_size = 48
-        self.HUD_font_size = 20
-        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen'/'Silkscreen-Bold.ttf'
+        self.speedup_scale = 1.1
+        self.scole_scale = 1.5
+        self.ship_limit = 3
        
+        self.initalize_dynamic_settings() 
 
-    def initialize_dynamic_settings(self):
-             """Initialize settings that change throughout the game."""
-             self.ship_speed = 5
-             self.starting_ship_count = 3 
 
-             self.bullet_w = 25
-             self.bullet_h = 80
-             self.bullet_speed = 7
-             self.bullet_amount = 5
-             
-             self.alien_speed = 80
-             self.fleet_drop_speed = 40
-             self.alien_points = 50
+    def initalize_dynamic_settings(self):
+        self.ship_speed = 3.0
+        self.alien_speed = 3.0
+        self.bullet_speed = 5.0
+        self.alien_points = 50
 
-    def increase_difficulty(self):
+        self.fleet_direction = 1
+
+
+    def increase_speed(self):
         """Increase game difficulty by adjusting speed."""
-        self.ship_speed *= 1.1
-        self.bullet_speed *= 1.15
-        self.alien_speed *= 1.3
-        self.fleet_drop_speed *= 1.30
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+       
