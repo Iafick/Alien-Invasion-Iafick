dc499b5e25a54cf8d63062816bb2d0da2108c981
Iafick <iafick@student.cscc.edu>
2026-07-30 20:36:40 -0400

    Refactored scoreboard.py to improve readability

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,6 @@
 import pygame.font
 from pygame.sprite import Group
-from ship import ship
+from ship import Ship
 
 
 class Scoreboard:
@@ -14,12 +14,14 @@ class Scoreboard:
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
 
+        # prep of scoreboard info
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
         self.prep_ship()
 
     def prep_score(self):
+        """Turn the current score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
         score_str = f"{rounded_score:,}" #100000 -> 1,000,000
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
@@ -29,6 +31,7 @@ class Scoreboard:
         self.score_rect.top = 20
 
     def prep_high_score(self):
+        """Turn high score into a rendered image. """
        high_score = round(self.stats.high_score, -1)
        high_score_str = f"{high_score:,}"
        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
@@ -38,17 +41,22 @@ class Scoreboard:
        self.high_score_rect.top = self.score_rect.top
 
     def show_score(self):
+        """ Draw all scoreboard infomation."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
         self.screen.blit(self.ship_image, self.ship_rect)
 
+        self.ships.draw(self.screen)
+
     def check_high_score(self):
+        """Update the high score when necessary."""
         if self.stats.score > self.stats.high_score:
          self.stats.high_score = self.stats.score
          self.prep_high_score()
 
     def prep_level(self):
+        """Turn the current level into a rendered image. """
        level_str = str(self.stats.level)
        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
 
@@ -57,7 +65,9 @@ class Scoreboard:
        self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_ship(self):
-        self.ship_image = Group()
+        """ Show one ship icon for every remaining life. """
+        self.ships = Group()
+        
         for ship_number in range(self.stats.ships_left):
             ship = Ship(self.ai_game)
             ship.rect.x = 10 + ship_number * ship.rect.width
