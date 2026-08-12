02aa859be26cebf60ff8377cde98c1d91cc168dc
Iafick <iafick@student.cscc.edu>
2026-08-11 20:45:20 -0400

    Refactored the alien movement and edge detection methods

--- alien.py
+++ alien.py
@@ -24,40 +24,35 @@ class Alien(Sprite):
 
     def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
-
         super().__init__()
-
         self.screen = ai_game.screen
         self.settings = ai_game.settings
 
+        # Load the alien image and rotate it to face upward.
         image_path = Path("Assets") / "images" / "enemy_4.png"
-
         image = pygame.image.load(image_path).convert_alpha()
-
-        # Rotate alien to face upward.
         self.image = pygame.transform.rotate(image, 180)
-
         self.rect = self.image.get_rect()
 
-        # Start at bottom of screen.
-        self.rect.midbottom = self.screen.get_rect().midbottom
+        # Start each new alien near the bottom left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.bottom = self.screen.get_rect().bottom
+
+        # Store the alien's exact horizontal and vertical position.
+        self.x = float(self.rect.x)
         self.y = float(self.rect.y)
 
-
-    def check_edge(self):
-        """Return True if alien reaches the player's side."""
-
+    def check_edges(self):
+        """Return True if alien is at edge of screen (left or right)."""
         screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
 
-        if self.rect.top <= screen_rect.top:
-            return True
-        return False
+    def check_top(self):
+        """Return True if alien reaches the player's side (top of screen)."""
+        screen_rect = self.screen.get_rect()
+        return self.rect.top <= screen_rect.top
 
     def update(self):
-        """Move alien upward."""
-
-        self.y -= self.settings.alien_speed
-
-        self.rect.y = int(self.y)
-
-
+        """Move alien right or left, per fleet_direction."""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
