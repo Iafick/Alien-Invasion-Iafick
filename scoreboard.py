487a9cf510868a9f89a0a8f6da964f6c73dcc026
Iafick <iafick@student.cscc.edu>
2026-08-12 14:54:04 -0400

    Add show_score method to display scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -24,6 +24,7 @@ class Scoreboard:
         """Turn the current score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
         score_str = f"{rounded_score:,}"
+
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.settings.bg_color
         )
@@ -35,6 +36,7 @@ class Scoreboard:
         """Turn high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
         high_score_str = f"{high_score:,}"
+
         self.high_score_image = self.font.render(
             high_score_str, True, self.text_color, self.settings.bg_color
         )
@@ -42,6 +44,7 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    
     def show_score(self):
         """Draw all scoreboard information."""
         self.screen.blit(self.score_image, self.score_rect)
